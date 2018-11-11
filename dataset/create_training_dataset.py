1eaf0fba8839b1a8b6041484149b63011a591d81
Unknown <lapsintra@gmail.com>
2018-11-11 01:00:47 +0000

    Dataset with additional cuts and friends

--- dataset/create_training_dataset.py
+++ dataset/create_training_dataset.py
@@ -38,72 +38,109 @@ def main(args, config):
             logger.debug("Collect events of process {} for fold {}.".format(
                 process, num_fold))
 
-            # Create output file
-            created_files.append(
-                os.path.join(config["output_path"],
-                             "merge_fold{}_{}.root".format(num_fold, process)))
-            file_ = ROOT.TFile(created_files[-1], "RECREATE")
+            # Collect all files for this process in chains
+            basepaths = []
+            basepaths.append(config["base_path"])
+            basepaths.extend(config["friend_dirs"])
 
-            # Collect all files for this process in a chain
-            chain = ROOT.TChain(config["tree_path"])
-            for filename in config["processes"][process]["files"]:
-                path = os.path.join(config["base_path"], filename)
-                if not os.path.exists(path):
-                    logger.fatal("File does not exist: {}".format(path))
-                chain.AddFile(path)
+            created_files.append([])
+            for i, basepath in enumerate(basepaths):
+                chains = []
+                for basepath in basepaths:
+                    chain = ROOT.TChain(config["tree_path"])
+                    for filename in config["processes"][process]["files"]:
+                        path = os.path.join(basepath, filename)
+                        if not os.path.exists(path):
+                            logger.fatal("File does not exist: {}".format(path))
+                        chain.AddFile(path)
+                    chains.append(chain)
+                # Create output files
+                if i == 0: # base chain
+                    created_files[-1].append(
+                        os.path.join(config["output_path"],
+                                     "merge_fold{}_{}.root".format(num_fold, process)))
+                else:
+                    created_files[-1].append(
+                        os.path.join(config["output_path"],
+                                     "{}_merge_fold{}_{}.root".format(config["friend_aliases"][i-1], num_fold, process)))
 
-            chain_numentries = chain.GetEntries()
-            if not chain_numentries > 0:
-                logger.fatal(
-                    "Chain (before skimming) does not contain any events.")
-                raise Exception
-            logger.debug("Found {} events for process {}.".format(
-                chain_numentries, process))
+                file_ = ROOT.TFile(created_files[-1][-1], "RECREATE")
 
-            # Skim the events with the cut string
-            cut_string = "({EVENT_BRANCH}%2=={NUM_FOLD})&&({CUT_STRING})".format(
-                EVENT_BRANCH=config["event_branch"],
-                NUM_FOLD=num_fold,
-                CUT_STRING=config["processes"][process]["cut_string"])
-            logger.debug("Skim events with cut string: {}".format(cut_string))
+                chain_numentries = chains[0].GetEntries()
+                if not chain_numentries > 0:
+                    logger.fatal(
+                        "Chain (before skimming) does not contain any events.")
+                    raise Exception
+                logger.debug("Found {} events for process {}.".format(
+                    chain_numentries, process))
 
-            chain_skimmed = chain.CopyTree(cut_string)
-            chain_skimmed_numentries = chain_skimmed.GetEntries()
-            if not chain_skimmed_numentries > 0:
-                logger.fatal(
-                    "Chain (after skimming) does not contain any events.")
-                raise Exception
-            logger.debug("Found {} events for process {} after skimming.".
-                         format(chain_skimmed_numentries, process))
+                for j, friend in enumerate(chains):
+                    if i != j:
+                        alias = config["friend_aliases"][j-1] if j else ""
+                        chains[i].AddFriend(friend, alias)
 
-            # Write training weight to new branch
-            logger.debug("Add training weights with weight string: {}".format(
-                config["processes"][process]["weight_string"]))
-            formula = ROOT.TTreeFormula(
-                "training_weight",
-                config["processes"][process]["weight_string"], chain_skimmed)
-            training_weight = array('f', [-999.0])
-            branch_training_weight = chain_skimmed.Branch(
-                config["training_weight_branch"], training_weight,
-                config["training_weight_branch"] + "/F")
-            for i_event in range(chain_skimmed.GetEntries()):
-                chain_skimmed.GetEntry(i_event)
-                training_weight[0] = formula.EvalInstance()
-                branch_training_weight.Fill()
+                # Skim the events with the cut string
+                cut_string = "({EVENT_BRANCH}%2=={NUM_FOLD})&&({CUT_STRING})".format(
+                    EVENT_BRANCH=config["event_branch"],
+                    NUM_FOLD=num_fold,
+                    CUT_STRING=config["processes"][process]["cut_string"])
+                if i:
+                    pattern = "{}.".format(config["friend_aliases"][i-1])
+                    cut_string = cut_string.replace(pattern, "")
+                logger.debug("Skim events with cut string: {}".format(cut_string))
 
-            # Rename chain to process name and write to output file
-            logger.debug("Write output file for this process and fold.")
-            chain_skimmed.SetName(config["processes"][process]["class"])
-            chain_skimmed.Write()
-            file_.Close()
+                chain_skimmed = chains[i].CopyTree(cut_string)
+                chain_skimmed_numentries = chain_skimmed.GetEntries()
+                if not chain_skimmed_numentries > 0:
+                    logger.fatal(
+                        "Chain (after skimming) does not contain any events.")
+                    raise Exception
+                logger.debug("Found {} events for process {} after skimming.".
+                             format(chain_skimmed_numentries, process))
+
+                flist = chains[i].GetListOfFriends()
+                if flist:
+                    flist.Clear()
+                for chain in chains:
+                    chain.Reset()
+
+                if i == 0: # base chain
+                    # Write training weight to new branch
+                    logger.debug("Add training weights with weight string: {}".format(
+                        config["processes"][process]["weight_string"]))
+                    formula = ROOT.TTreeFormula(
+                        "training_weight",
+                        config["processes"][process]["weight_string"], chain_skimmed)
+                    training_weight = array('f', [-999.0])
+                    branch_training_weight = chain_skimmed.Branch(
+                        config["training_weight_branch"], training_weight,
+                        config["training_weight_branch"] + "/F")
+                    for i_event in range(chain_skimmed.GetEntries()):
+                        chain_skimmed.GetEntry(i_event)
+                        training_weight[0] = formula.EvalInstance()
+                        branch_training_weight.Fill()
+
+                # Rename chain to process name and write to output file
+                logger.debug("Write output file for this process and fold.")
+                chain_skimmed.SetName(config["processes"][process]["class"])
+                chain_skimmed.Write()
+                file_.Close()
+
 
         # Combine all skimmed files using `hadd`
         logger.debug("Call `hadd` to combine files of processes for fold {}.".
                      format(num_fold))
-        output_file = os.path.join(config["output_path"], "fold{}_{}".format(
-            num_fold, config["output_filename"]))
-        subprocess.call(["hadd", "-f", output_file] + created_files)
-        logger.info("Created output file: {}".format(output_file))
+        for i, chain in enumerate(chains):
+            if i == 0:
+                output_file = os.path.join(config["output_path"], "fold{}_{}".format(
+                    num_fold, config["output_filename"]))
+            else:
+                output_file = os.path.join(config["output_path"], "{}_fold{}_{}".format(
+                    config["friend_aliases"][i-1], num_fold, config["output_filename"]))
+            sub_created_files = [process_files[i] for process_files in created_files]
+            print(["hadd", "-f", output_file] + sub_created_files)
+            subprocess.call(["hadd", "-f", output_file] + sub_created_files)
+            logger.info("Created output file: {}".format(output_file))
 
 
 if __name__ == "__main__":
